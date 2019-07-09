f172f68ca97161f88cc6394dc7f7739a4713068f
gitmko0 <51514008+gitmko0@users.noreply.github.com>
2019-07-09 06:54:18 +0000

    Update gitmko0.go

--- gitmko0.go
+++ gitmko0.go
@@ -2,8 +2,21 @@ package gitmko0
 
 import (
      "time"
+     "bytes"
+     "encoding/binary"
 )
 
-func GetTimeInSeconds() uint64 {
+func GetTimestampSeconds() uint64 {
+     return uint64(time.Now().UnixNano() / int64(time.Second))
+}
+
+func GetTimestampMilliseconds() uint64 {
      return uint64(time.Now().UnixNano() / int64(time.Millisecond))
+}
+
+func GetTimestampBytes() []byte{
+        buf := new(bytes.Buffer)
+        binary.Write(buf, binary.LittleEndian, time.Now().UTC().Unix())
+        buf.Truncate(5)
+        return buf.Bytes() 
 }
